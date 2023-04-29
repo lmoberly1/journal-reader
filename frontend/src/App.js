4600aed93f9dbcbba31cd73e36b41dc7fa228e80
lmoberly1 <lukemoberly@gmail.com>
2023-04-29 15:12:09 -0500

    Add image upload test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import OCRUploader from "./components/OCRUploader";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Testing frontend deployment on Vercel</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Demo for uploading image</h1>
+      <OCRUploader />
     </div>
   );
 }
---
+++ frontend/src/components/OCRUploader.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const OCRUploader = () => {
+  const [file, setFile] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const handleFileChange = (e) => {
+    console.log("set file");
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+  };
+
+  const handlePost = async () => {
+    const url = "https://freeocrapi.com/api";
+    const data = new FormData();
+    data.append("file", file);
+
+    try {
+      const response = await fetch(url, {
+        mode: "no-cors",
+        method: "POST",
+        body: data,
+      });
+      console.log(data);
+      console.log("response", response);
+      const text = await response.text();
+      setResult(text);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      {file ? (
+        <button onClick={handlePost}>Post</button>
+      ) : (
+        <p>Select an image file to upload</p>
+      )}
+      {result ? <pre>{result}</pre> : null}
+    </div>
+  );
+};
+
+export default OCRUploader;
